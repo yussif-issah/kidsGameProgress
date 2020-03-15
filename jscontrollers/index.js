ceea042a19b324fe692a557e06e1c4573a68e816
yussif-issah <57133833+yussif-issah@users.noreply.github.com>
2020-03-15 16:09:07 +0000

    level2added

--- jscontrollers/index.js
+++ jscontrollers/index.js
@@ -2,12 +2,10 @@ const {app,BrowserWindow}=require('electron')
 const path=require('path')
 const url=require('url')
 const { ipcMain,Menu } = require('electron')
-const {remote}=require('electron')
-const {ipcRender}=require('electron')  
-let win
-var max_height=process.env.height;
-var max_width=process.env.width;
 
+let win
+let levelTwoWindow
+let values
 app.on('ready',function(){
     win=new BrowserWindow({width:1100,height:700,webPreferences: {
         nodeIntegration: true
@@ -23,10 +21,53 @@ app.on('ready',function(){
    win.on('closed',()=>{
      win=null
  })
- win.on("window-all-closed",()=>{
+ win.on("ready-to-show",()=>{
+    win.show()
+ })
+ //win.hide()
+ /*win.on("window-all-closed",()=>{
     app.quit()
     if(process.platform!="darwin"){
         app.quit()
     }
+})*/
+ipcMain.on('nextLevel',(err,args)=>{
+    CreateLevelTwoWindow()
+    values=args
+    win.close()
 })
 })
+
+function CreateLevelTwoWindow(){
+    levelTwoWindow=new BrowserWindow({width:880,height:680,webPreferences: {
+        nodeIntegration: true
+    }})
+    levelTwoWindow.loadURL(url.format({
+        pathname:path.join(__dirname,'../html/level_two.html'),
+        protocol:'file',
+        slashes:true
+    }))
+
+    levelTwoWindow.removeMenu()
+    levelTwoWindow.openDevTools()
+    levelTwoWindow.maximize()
+    
+ipcMain.on('loaded',(err,args)=>{
+    levelTwoWindow.webContents.send("values",values)
+})
+
+
+    levelTwoWindow.webContents.send('data',values)
+
+    levelTwoWindow.on("window-all-closed",()=>{
+        app.quit()
+        if(process.platform!="darwin"){
+            app.quit()
+        }
+    })
+
+    
+    levelTwoWindow.on('closed',()=>{
+     levelTwoWindow=null
+ })
+}
--- jscontrollers/level_two.js
+++ jscontrollers/level_two.js
@@ -1,27 +1,28 @@
-const {remote}=require('electron')
-const {ipcRenderer}=require('electron')
+const{ipcRenderer,remote}=require("electron")
+
 var questionNumber=1
 var score=0;
 var control=0;
 var timer;
 var attempt=0;
 var answered=0;
-var performance;
 function killgame(){
 var win =remote.getCurrentWindow()
 win.close()
 }
 
- document.getElementById("noise").addEventListener('click',()=>{
-     var box=document.getElementById('noise')
-     if(box.checked==true){
-         document.getElementById("backgroundsong").pause()
-         document.getElementById("fornoise").innerHTML="play music"
-     }else{
-        document.getElementById("backgroundsong").play()
-        document.getElementById("fornoise").innerHTML="stop music"
-     }
- })
+document.getElementById("noise").addEventListener('click',()=>{
+    var box=document.getElementById('noise')
+    if(box.checked==true){
+        document.getElementById("backgroundsong").pause()
+        document.getElementById("fornoise").innerHTML="play music"
+    }else{
+       document.getElementById("backgroundsong").play()
+       document.getElementById("fornoise").innerHTML="stop music"
+    }
+})
+
+
 
 
 function pop(){
@@ -48,11 +49,15 @@ function playgame(){
 
 function questionOne() {
     var qOne=`<div class="questionarea">
-    <h1 style="color:red;" id="head">QUESTION ONE</h1>
+    <h1 id="head" style="color:red;">QUESTION ONE</h1>
     <p>let a=20</p>
-    <p>let b=30</p>
-    <p>let c=a+b</p>
-    <h5>what is the value of c</h5><input type="text" placeholder="ANSWER HERE" id="inputanswere">
+    <p>let c=any number</p>    
+    <p id="hint">if(c>a){</p>
+    <p id="hint">print("christiano ronaldooooo")</p>
+    <p id="hint">}else{</p>
+    <p id="hint">print("leonel messiiii")</p>
+    <p id="hint">}</p>
+    <h5>if the program printed "christiano ronaldooooo" what is the appropriate value of c</h5><input type="text" placeholder="ANSWER HERE" id="inputanswere">
     <button id="btn" onclick="Answer()">ANSWER</button><span><button id="btn" onclick="checkAnswer()">CHECK ANSWER</button></span>
     <p id="checkans"></p>
     </div>`
@@ -68,17 +73,14 @@ function questionOne() {
 function Answer(){
   var ans=  document.getElementById("inputanswere").value;
   if(questionNumber==1){
-  if(ans==50){
+  if(ans>20){
       correctAnswer()
-      answered+=1
       checkNumberOfQuestionAnswered(1)
-      calculatePerformance()
   }else{
       wrongAnswer()
-      calculatePerformance()
   }
 }else if(questionNumber==2){
- if(ans==70){
+ if(ans!=20){
      correctAnswer()
      answered+=1
      checkNumberOfQuestionAnswered(2)
@@ -88,7 +90,7 @@ function Answer(){
      calculatePerformance()
  }
 }else if(questionNumber==3){
-    if(ans==5){
+    if(ans=='a'||ans=='A'){
         correctAnswer()
         answered+=1
         checkNumberOfQuestionAnswered(3)
@@ -98,18 +100,17 @@ function Answer(){
         calculatePerformance()
     }
 }else if(questionNumber==4){
-    if(ans==100){
+    if(ans=='b'||ans=='B'){
         correctAnswer()
         answered+=1
         checkNumberOfQuestionAnswered(4)
         calculatePerformance()
-        
     }else{
-        wrongAnswer()
+        wrongAnswer
         calculatePerformance()
     }
 }else if(questionNumber==5){
-    if(ans==100){
+    if(ans=='a'||ans=='A'){
         correctAnswer()
         answered+=1
         checkNumberOfQuestionAnswered(5)
@@ -117,6 +118,7 @@ function Answer(){
         congrats()
     }else{
         wrongAnswer()
+        calculatePerformance()
     }
 }
 }
@@ -124,8 +126,7 @@ function Answer(){
 function congrats(){
     var q=`<div class="questionarea">
     <h1 style="color:red;">CONGRATS ,YOU HAVE COMPLETED THIS LEVEL</h1>
-    <img src="../resources/images/school1.gif" alt="pic">
-    <button id="btn" onclick="nextLevel()">GO TO NEXT LEVEL</button>
+    <img src="../resources/images/applaud.gif" alt="pic">
     </div>`
 document.getElementById("questionsArea").innerHTML=q
 }
@@ -142,10 +143,7 @@ function attempted(){
  document.getElementById("attemptvalue").innerHTML=attempt
 }
 
-function calculatePerformance(){
-    performance=(answered/attempt)*100
-    document.getElementById("performance").innerHTML=performance.toFixed(0)+"%"
-}
+
 
 function correctAnswer(){
     document.getElementById("correct").play()
@@ -179,11 +177,15 @@ function increaseScore(){
 
 function questionTwo(){
     var qOne=`<div class="questionarea">
-    <h1 style="color:blue;" id="head">QUESTION TWO</h1>
-    <p>let a=100</p>
-    <p>let b=30</p>
-    <p>let c=a-b</p>
-    <p>what is the value of c</p><input type="text" placeholder="anwere here" id="inputanswere">
+    <h1 id="head" style="color:blue;">QUESTION TWO</h1>
+    <p>let a=20</p>
+    <p>let c=any number</p>    
+    <p id="hint">if(c==a){</p>
+    <p id="hint">print("christiano ronaldoooo")</p>
+    <p id="hint">}else{</p>
+    <p id="hint">print("leonel messiiii")</p>
+    <p id="hint">}</p>
+    <p>what is the appropriate value of c if the program printed "leonel messiiii"</p><input type="text" placeholder="anwere here" id="inputanswere">
     <button id="btn" onclick="Answer()">ANSWER</button><span><button id="btn" onclick="checkAnswer()">CHECK ANSWER</button></span>
     <p id="checkans"></p>
     </div>`
@@ -197,11 +199,18 @@ function questionTwo(){
 
 function questionThree(){
     var qOne=`<div class="questionarea">
-    <h1 style="color:green;" id="head">QUESTION THREE</h1>
-    <p>let a=100</p>
-    <p>let b=20</p>
-    <p>let c=a/b</p>
-    <p>what is the value of c</p><input type="text" placeholder="anwere here" id="inputanswere">
+    <h1 id="head" style="color:green;">QUESTION THREE</h1>
+    <p>let a=20</p>
+    <p>let c=100</p>    
+    <p id="hint">if(condition){</p>
+    <p id="hint">print("christiano ronaldoooo")</p>
+    <p id="hint">}else{</p>
+    <p id="hint">print("leonel messiiii")</p>
+    <p id="hint">}</p>
+    <p>what condition statement will make the program print "christiano ronaldooo"</p>
+    <h3>A: c>a</h3>
+    <h3 style="margin:5px">B: c==a</h3>
+    <input type="text" placeholder="anwere here" id="inputanswere">
     <button id="btn" onclick="Answer()">ANSWER</button><span><button id="btn" onclick="checkAnswer()">CHECK ANSWER</button></span>
     <p id="checkans"></p>
     </div>`
@@ -214,32 +223,40 @@ function questionThree(){
 
 
 function questionFour(){
-        var qOne=`<div class="questionarea">
-        <h1 style="color:red;" id="head">QUESTION FOUR</h1>
-        <p>let a=10</p>
-        <p>let b=10</p>
-        <p>let c=axb</p>
-        <p>what is the value of c</p><input type="text" placeholder="anwere here" id="inputanswere">
-        <button id="btn" onclick="Answer()">ANSWER</button><span><button id="btn" onclick="checkAnswer()">CHECK ANSWER</button></span>
-        <p id="checkans"></p>
-        </div>`
-        document.getElementById("questionsArea").innerHTML=qOne
-        document.getElementById("startgame").style.display='none'
-        document.getElementById("div1").style.marginTop='25%'
-        document.getElementById("div1").style.marginLeft='35%'
-        document.getElementById("div1").style.width='100%'
+    var qOne=`<div class="questionarea">
+    <h1 id="head"  style="color:red;">QUESTION FOUR</h1>
+    <p>let a=100</p>
+    <p>let c=100</p>    
+    <p id="hint">if(condition){</p>
+    <p id="hint">print("christiano ronaldoooo")</p>
+    <p id="hint">}else{</p>
+    <p id="hint">print("leonel messiiii")</p>
+    <p id="hint">}</p>
+    <p>what condition statement will make the program print "christiano ronaldooo"</p>
+    <h3>A: c>a</h3>
+    <h3 style="margin:5px">B: c==a</h3>
+    <input type="text" placeholder="anwere here" id="inputanswere">
+    <button id="btn" onclick="Answer()">ANSWER</button><span><button id="btn" onclick="checkAnswer()">CHECK ANSWER</button></span>
+    <p id="checkans"></p>
+    </div>`
+    document.getElementById("questionsArea").innerHTML=qOne
+    document.getElementById("startgame").style.display='none'
+    document.getElementById("div1").style.marginTop='25%'
+    document.getElementById("div1").style.marginLeft='35%'
+    document.getElementById("div1").style.width='100%'
 }
 
 
 function questionFive(){
         var qOne=`<div class="questionarea">
-        <h1 style="color:yellow;" id="head">LAST QUESTION</h1>
-        <p>let a=20</p>
-        <p>let b=10</p>
-        <p>let c=a+b</p>
-        <p>let d=c-a</p>
-        <p>let e=dxb</>
-        <p>what is the value of e</p><input type="text" placeholder="anwere here" id="inputanswere">
+        <h1 id="head" style="color:yellow;">LAST QUESTION</h1>
+        <p>let a=200</p>
+        <p>let b=100</p>  
+        <p>which of the following conditions will evaluate to true</p> 
+        <h5>A: a>b</h5>
+        <h5>B: a'<'b</h5>
+        <h5>C: a==b</h5>
+        </p><input type="text" placeholder="anwere here" id="inputanswere">
         <button id="btn" onclick="Answer()">ANSWER</button><span><button id="btnans" onclick="checkAnswer()">CHECK ANSWER</button></span>
         <p id="checkans"></p>
         </div>`
@@ -258,21 +275,20 @@ function checkNumberOfQuestionAnswered(number){
 
 function checkAnswer(){
     if(questionNumber==1){
-        toggle("ANSWER=50")
+        toggle("ANY NUMBER GREATER THAN 20 eg 30,40 etc")
     }else if(questionNumber==2){
-        toggle("ANSWER=70")
+        toggle("ANY NUMBER APART FROM 20 eg 1,0,50")
     }else if(questionNumber==3){
-        toggle("ANSWER=5")
+        toggle("A")
     }else if(questionNumber==4){
-        toggle("ANSWER=100")
+        toggle("B")
     }else{
-        toggle("ANSWER=100")
+        toggle("A")
     }
 }
 
 function toggle(Qans){
    if(document.getElementById("checkans").innerHTML==Qans){
-       answered+=1
     document.getElementById("checkans").innerHTML="null"
     document.getElementById("checkans").style.display='none'
    }else{
@@ -281,6 +297,23 @@ function toggle(Qans){
 }
 
 function nextLevel(){
-    var values={"attempt":attempt,"score":score,"answers":answered}
-    ipcRenderer.send("nextLevel",values)
+    ipcRenderer.send("nextLevel")
+}
+
+ipcRenderer.on("values",(error,args)=>{
+    score=args.score
+    attempt=args.attempt
+    answered=args.answers
+    document.getElementById("attemptvalue").innerHTML=attempt
+    document.getElementById("scorevalue").innerHTML=score
+    document.getElementById("performance").innerHTML=((answered/attempt)*100).toFixed(0)+"%"
+})
+
+function loaded(){
+    ipcRenderer.send('loaded')
+}
+
+function calculatePerformance(){
+    performance=(answered/attempt)*100
+    document.getElementById("performance").innerHTML=performance.toFixed(0)+"%"
 }
